dcbd200ba4e90e87a5b19a46e873209d989b4a85
VeyrStudio <cvyctorea@gmail.com>
2026-08-27 04:39:28 -0400

    Harden The Library v1.0.16 drag-drop patch matching

--- tools/the-library-wrap-drop-multiselect-v116.py
+++ tools/the-library-wrap-drop-multiselect-v116.py
@@ -34,20 +34,6 @@ try{
     if(-not $text.Contains($marker)){
         if(-not $text.Contains('function Register-CoverDropTarget')){throw 'Could not find The Library drag-and-drop handler.'}
 
-        $old=@'
-    $Control.Add_DragDrop({
-        param($sender,$e)
-        $files = [string[]]$e.Data.GetData([System.Windows.Forms.DataFormats]::FileDrop)
-        if ($sender.Tag -eq "single") {
-            $panelSingleDrop.BackColor = [System.Drawing.Color]::FromArgb(64, 18, 96)
-            if ($files.Count -gt 0) { Set-SingleDroppedFile $files[0] }
-        } else {
-            $panelWrapDrop.BackColor = [System.Drawing.Color]::FromArgb(64, 18, 96)
-            if ($files.Count -gt 0) { Set-WrapDroppedFile $files[0] }
-        }
-    })
-'@
-
         $new=@'
     # WRAP DROP MULTISELECT FIX v1.0.16
     $Control.Add_DragDrop({
@@ -68,11 +54,9 @@ try{
             )
 
             if ($valid.Count -eq 1) {
-                # One dropped wrap keeps the normal single-cover splitter.
                 Set-WrapDroppedFile $valid[0]
             }
             elseif ($valid.Count -gt 1) {
-                # Multiple dropped wraps go directly into the batch splitter.
                 if (Get-Command Show-LibraryUnifiedWrapPaths -ErrorAction SilentlyContinue) {
                     Show-LibraryUnifiedWrapPaths -Paths $valid
                 }
@@ -87,8 +71,21 @@ try{
     })
 '@
 
-        if(-not $text.Contains($old)){throw 'Could not find the current one-file-only wrap drop handler.'}
-        $text=$text.Replace($old,$new)
+        $functionStart=$text.IndexOf('function Register-CoverDropTarget')
+        $dragStart=$text.IndexOf('$Control.Add_DragDrop({',$functionStart)
+        if($functionStart -lt 0 -or $dragStart -lt 0){throw 'Could not find the current drag-and-drop event block.'}
+
+        $dragClose=$text.IndexOf('    })',$dragStart)
+        if($dragClose -lt 0){
+            $dragClose=$text.IndexOf('})',$dragStart)
+        }
+        if($dragClose -lt 0){throw 'Could not find the end of the current drag-and-drop event block.'}
+
+        $dragEnd=$dragClose
+        while($dragEnd -lt $text.Length -and $text[$dragEnd] -ne [char]10){$dragEnd++}
+        if($dragEnd -lt $text.Length){$dragEnd++}
+
+        $text=$text.Substring(0,$dragStart)+$new.TrimStart()+$text.Substring($dragEnd)
     }
 
     [IO.File]::WriteAllText($targetMain,$text,(New-Object Text.UTF8Encoding($true)))
